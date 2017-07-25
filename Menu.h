b573154f999bdd6c81d901f07f9e8483f45175ac
theapprenticewizard <rayd3900@gmail.com>
2017-07-25 16:23:46 -0400

    fixed last commit

--- Menu.h
+++ Menu.h
@@ -10,17 +10,30 @@ class IMenuItem
 {
     private:
         const string m_text;
+        const string m_use_key;
     public:
-        IMenuItem(string text) : m_text(text) {};
-        virtual void execute();
+        IMenuItem(const char* text) : m_text(text){};
+        virtual void Execute();
 };
 
+class ExitMenuItem : public IMenuItem
+{
+    using IMenuItem::IMenuItem;
+
+    public:
+        void Execute();
+};
 
 class Menu 
 {
     private:
         vector<IMenuItem> m_menu_items;
     public:
+        Menu() 
+        {
+            m_menu_items.push_back(ExitMenuItem("Exit"));
+        }
+        void Display();
 };
 
 #endif
