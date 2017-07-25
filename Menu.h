201a8ef493cb602bf8412ed2b393969077da8f47
theapprenticewizard <rayd3900@gmail.com>
2017-07-25 16:23:14 -0400

    added abstractions for the menu and added a player class:

--- Menu.h
+++ Menu.h
@@ -1,6 +1,26 @@
+#ifndef MENU
+#define MENU
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class IMenuItem
+{
+    private:
+        const string m_text;
+    public:
+        IMenuItem(string text) : m_text(text) {};
+        virtual void execute();
+};
+
+
 class Menu 
 {
     private:
-
+        vector<IMenuItem> m_menu_items;
     public:
 };
+
+#endif
