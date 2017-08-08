3aecba0403192235c921e62cbeeea8814a3e00a2
theapprenticewizard <rayd3900@gmail.com>
2017-08-08 16:13:31 -0400

    added player functionality

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,31 @@
 #include "Player.h"
 
-Player::Player()
+Player::Player() {} // do nothing
+
+void Player::award_points(int points)
 {
-    
+    m_points += points;
+
+    if (points > 120) 
+       m_has_won = true;
+}
+
+void Player::set_dealer(bool is_dealer)
+{
+    m_is_dealer = is_dealer;
+}
+
+bool Player::is_dealer() 
+{
+    return m_is_dealer;
+}
+
+bool Player::has_won()
+{
+    return m_has_won;
+}
+
+int Player::get_points() 
+{
+    return m_points;
 }
--- Player.h
+++ Player.h
@@ -11,14 +11,20 @@ using namespace std;
 class Player
 {   
     private:
-        vector<Card> hand;
-        vector<Card> crib;
-        string name;
-        bool is_dealer;
+        vector<Card> m_hand;
+        vector<Card> m_crib;
+        string m_name;
+        int m_points = 0;
+        bool m_is_dealer = false;
+        bool m_has_won = false;
     public:
         Player();
-        Player(string m_name) : name(m_name) {};
+        Player(string name) : m_name(name) {};
         void award_points(int points);
+        void set_dealer(bool is_dealer);
+        bool is_dealer();
+        bool has_won();
+        int get_points();
 };
 
 #endif
